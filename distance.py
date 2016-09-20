0272fba443b71d08e360039e4d25f00bd8ae0cde
seananchabra <seananchabra@hotmail.com>
2016-09-19 23:21:36 -0400

    Update distance.py

--- distance.py
+++ distance.py
@@ -1,13 +1,12 @@
 import googlemaps
 from datetime import datetime
 
-def distance(self, origin, destination)
-  self.origin = origin
-  self.destination = destination
-  
+def distance(orig, dest):
+  origin = orig
+  destination = dest
   gmaps = googlemaps.Client(key='AIzaSyDBubBQL2tNO9bKjv5gYOLBcCeRghAOi7o')
-  distance-matrix = gmaps.distance_matrix(self.origin, self.destination)
-  
-  return distance-matrix['rows'][0]['elements'][0]['distance']['text']
+  distance_matrix = gmaps.distance_matrix(origin, destination)
+  return (distance_matrix['rows'][0]['elements'][0]['distance']['text'])
 
-distance("Ryerson University, Toronto ON", "Harvard University, Cambridge MA")
+#enter the origin and destination of the place you wish to recieve a distance of
+print (distance("Ryerson University, Toronto ON", "Harvard University, Cambridge MA"))
